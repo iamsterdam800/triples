2a7d890515254ba329f752c91f976f754d4cad8a
iamsterdam800 <iamsterdam800@users.noreply.github.com>
2021-06-08 14:08:58 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.88d213a13c1eef194d13e7c6a1df9733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc47cb2d91f3c7fe3e3bb492a07e0fdf",
+    "url": "/triples/index.html"
+  },
+  {
+    "revision": "25e89dd359efa04bd756",
+    "url": "/triples/static/css/2.eac53682.chunk.css"
+  },
+  {
+    "revision": "95c50704212c4415a512",
+    "url": "/triples/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "25e89dd359efa04bd756",
+    "url": "/triples/static/js/2.e5106de0.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/triples/static/js/2.e5106de0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95c50704212c4415a512",
+    "url": "/triples/static/js/main.1f57ba45.chunk.js"
+  },
+  {
+    "revision": "f3618478bd73d6d48cab",
+    "url": "/triples/static/js/runtime-main.eac1e1ea.js"
+  }
+]);
